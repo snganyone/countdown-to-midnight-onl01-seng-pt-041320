f22089bc8cf8abd65c569f35e849a0b49308c5df
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-16 00:16:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
 #write your code here
+def countdown_with_sleep(number)
+  sleep(number)
+end
+
 def countdown(number)
   while n > 0
     puts "${number} SECOND(S)!"
     n -= 1
   end
   puts "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(number)
-  sleep(number)
 end
