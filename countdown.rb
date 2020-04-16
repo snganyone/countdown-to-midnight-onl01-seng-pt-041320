8a724b3c2f8481d54214b03ab89c508ac39b0321
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-16 00:29:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ def countdown(number)
   while number > 0
     countdown_with_sleep(number)
     puts "${number} SECOND(S)!"
+    puts "HAPPY NEW YEAR!"
     n -= 1
   end
-  puts "HAPPY NEW YEAR!"
 end
